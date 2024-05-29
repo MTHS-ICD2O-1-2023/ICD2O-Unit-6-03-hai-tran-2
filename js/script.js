1c0272bc7c8d9c3bf41fbedf00d9876c07765052
Hai Tran <159185497+hai-tran-2@users.noreply.github.com>
2024-05-29 14:41:04 +0000

    add storyboard and flowchart

--- js/script.js
+++ js/script.js
@@ -10,11 +10,10 @@
  * Get API info.
  */
 
-async function showCurrentTemperature() {
+async function showCurrentWeather() {
   try {
     const resultJSON = await fetch("https://api.openweathermap.org/data/2.5/weather?lat=45.4211435&lon=-75.6900574&appid=fe1d80e1e103cff8c6afd190cad23fa5")
     const jsonData = await resultJSON.json()
-    //console.log(jsonData)
     const currentWeather = jsonData["weather"][0]["description"]
     const weatherIcon = jsonData["weather"][0]["icon"]
     const urlAddress = "https://openweathermap.org/img/wn/" + weatherIcon + "@2x.png"
